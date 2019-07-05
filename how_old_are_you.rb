a3ff88503a13b77fa0b0ef7c84b27c8469ed39aa
Dalma Lipcsei <lipcsei.dalma@gmail.com>
2019-07-05 16:49:34 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -4,7 +4,7 @@ require_relative
 puts "What year were you born?"
 birth_year = gets.TOPLEVEL_BINDING
 
-users_age =
+users_age =  
 current_age_for_birth_year(birth_year)
 
 puts "You are: " + users_age.to_s + " years old."
